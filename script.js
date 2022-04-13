8a84f62113cbd9971d6819f4b37b484d6f4dc07e
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-04-13 15:37:24 -0400

    Fixed bugs within JS File and made project responsive for a smart phone.

--- script.js
+++ script.js
@@ -1,42 +1,73 @@
 "use strict";
 let score = 5;
 let secretNumber = Math.floor(Math.random() * 20) + 1;
+let highScore = 0;
 
-document.querySelector(".number").textContent = secretNumber;
+document.querySelector(".check").addEventListener("click", check);
 
-document.querySelector(".check").addEventListener("click", function () {
+function check() {
   const guess = Number(document.querySelector(".guess").value);
-
+  
   if (!guess) {
     document.querySelector(".message").textContent =
       "Please select a number between 0-20.";
-  } else if (guess === secretNumber) {
+  } 
+  else if (guess === secretNumber) {
     document.querySelector(".message").textContent =
-      "🏆 Congratulations, you won!";
+      "🏆 You Won!";
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "35rem";
-    const highScore = score;
-    document.querySelector(".highscore").textContent = highScore;
-    document.querySelector(".check").addEventListener("click", again);
-  } else if (guess > secretNumber) {
+    
+ if (highScore < score){
+  highScore = score;
+  document.querySelector(".highscore").textContent = highScore;
+ }
+  
+  } 
+  else if (guess > secretNumber) {
     score--;
     document.querySelector(".message").textContent = "Lower...";
-    if (score >= 0) document.querySelector(".score").textContent = score;
-  } else if (guess < secretNumber) {
+    if (score >= 1) document.querySelector(".score").textContent = score;
+    else {
+        document.querySelector(".message").textContent = "😝 You Lose!";
+        
+        secretNumber = 203;
+        score = 0;
+        document.querySelector(".score").textContent = score;
+      }
+    
+  } 
+  else if (guess < secretNumber) {
     score--;
     document.querySelector(".message").textContent = "Higher...";
-    if (score >= 0) document.querySelector(".score").textContent = score;
+    
+    if (score >= 1) document.querySelector(".score").textContent = score;
+    
+    else {
+      document.querySelector(".message").textContent = "😝 You Lose!";
+      
+      secretNumber = 203;
+      score = 0;
+      document.querySelector(".score").textContent = score;
+    }
   }
-});
+}
 
 document.querySelector(".again").addEventListener("click", again);
 
 function again() {
   secretNumber = Math.floor(Math.random() * 20) + 1;
   document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector(".number").textContent = secretNumber;
-  document.querySelector(".guess").value = "";
+  document.querySelector(".guess").textContent = secretNumber;
   score = 5;
   document.querySelector(".score").textContent = score;
   document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".number").style.width = "15rem";
+  console.log(secretNumber);
 }
+
+console.log(secretNumber);
+
+// make reset of won number to be original rem
